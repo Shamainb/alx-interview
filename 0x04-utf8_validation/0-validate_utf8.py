2ddf198a41bf838d3d0d357c701c686dafe0c005
Shamainb <shamainbhengesa60@gmail.com>
2024-05-23 22:21:46 +0200

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def validUTF8(data):
     def get_num_of_bytes(byte):
         if byte & 0b10000000 == 0:
@@ -12,15 +13,15 @@ def validUTF8(data):
             return 4
         else:
             return -1
-    
-    i = 0
-    while i < len(data):
-        num_of_bytes = get_num_of_bytes(data[i])
-        if num_of_bytes == -1 or i + num_of_bytes > len(data):
-            return False
-        for j in range(1, num_of_bytes):
-            if data[i + j] & 0b11000000 != 0b10000000:
+
+        i = 0
+        while i < len(data):
+            num_of_bytes = get_num_of_bytes(data[i])
+            if num_of_bytes == -1 or i + num_of_bytes > len(data):
                 return False
-        i += num_of_bytes
-    
-    return True
+            for j in range(1, num_of_bytes):
+                if data[i + j] & 0b11000000 != 0b10000000:
+                    return False
+                i += num_of_bytes
+
+                return True
