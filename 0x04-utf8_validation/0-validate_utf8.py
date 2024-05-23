fbca12674d9aa1849004d4bca23087d12053935c
Shamainb <shamainbhengesa60@gmail.com>
2024-05-23 22:11:07 +0200

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+def validUTF8(data):
+    def get_num_of_bytes(byte):
+        if byte & 0b10000000 == 0:
+            return 1
+        elif byte & 0b11100000 == 0b11000000:
+            return 2
+        elif byte & 0b11110000 == 0b11100000:
+            return 3
+        elif byte & 0b11111000 == 0b11110000:
+            return 4
+        else:
+            return -1
+    
+    i = 0
+    while i < len(data):
+        num_of_bytes = get_num_of_bytes(data[i])
+        if num_of_bytes == -1 or i + num_of_bytes > len(data):
+            return False
+        for j in range(1, num_of_bytes):
+            if data[i + j] & 0b11000000 != 0b10000000:
+                return False
+        i += num_of_bytes
+    
+    return True
