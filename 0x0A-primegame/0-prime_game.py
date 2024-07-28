c7b1e354d34ac2927fbda640d9d9b6306a51217c
Shamainb <shamainbhengesa60@gmail.com>
2024-07-28 03:21:26 +0200

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,7 +7,7 @@ def isWinner(x, nums):
     winnerCounter = {'Maria': 0, 'Ben': 0}
 
     for i in range(x):
-        roundWinner = isRoundWinner(nums[i], x)
+        roundWinner = isRoundWinner(nums[i])
         if roundWinner is not None:
             winnerCounter[roundWinner] += 1
 
@@ -19,48 +19,38 @@ def isWinner(x, nums):
         return None
 
 
-def isRoundWinner(n, x):
+def isRoundWinner(n):
     '''find round winner'''
     list = [i for i in range(1, n + 1)]
     players = ['Maria', 'Ben']
+    currentPlayerIndex = 0
 
-    for i in range(n):
-        # get current player
-        currentPlayer = players[i % 2]
-        selectedIdxs = []
-        prime = -1
-        for idx, num in enumerate(list):
-            # if already picked prime num then
-            # find if num is multipl of the prime num
-            if prime != -1:
-                if num % prime == 0:
-                    selectedIdxs.append(idx)
-            # else check is num is prime then pick it
-            else:
-                if isPrime(num):
-                    selectedIdxs.append(idx)
-                    prime = num
-        # if failed to pick then current player lost
-        if prime == -1:
-            if currentPlayer == players[0]:
-                return players[1]
-            else:
-                return players[0]
-        else:
-            for idx, val in enumerate(selectedIdxs):
-                del list[val - idx]
-    return None
+    while True:
+        currentPlayer = players[currentPlayerIndex]
+        madeMove = False
+        for num in list:
+            if isPrime(num):
+                list = [x for x in list if x % num != 0]
+                madeMove = True
+                break
+
+        if not madeMove:
+            return players[1 - currentPlayerIndex]
+
+        currentPlayerIndex = 1 - currentPlayerIndex
 
 
 def isPrime(n):
-    # 0, 1, even numbers greater than 2 are NOT PRIME
-    if n == 1 or n == 0 or (n % 2 == 0 and n > 2):
+    '''check if number is prime'''
+    if n <= 1:
         return False
-    else:
-        # Not prime if divisable by another number less
-        # or equal to the square root of itself.
-        # n**(1/2) returns square root of n
-        for i in range(3, int(n**(1/2))+1, 2):
-            if n % i == 0:
-                return "Not prime"
+    if n <= 3:
         return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
