c59ca4b9991c73240569302fb552e1a14eb4287a
Shamainb <shamainbhengesa60@gmail.com>
2024-07-28 03:28:47 +0200

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,12 +3,17 @@
 
 
 def isWinner(x, nums):
-    '''finds the winner'''
+    '''Determines the winner after x rounds'''
+    if x < 1 or not nums:
+        return None
+
+    # Counter for the wins
     winnerCounter = {'Maria': 0, 'Ben': 0}
 
-    for i in range(x):
-        roundWinner = isRoundWinner(nums[i])
-        if roundWinner is not None:
+    for n in nums:
+        # Find the round winner
+        roundWinner = isRoundWinner(n)
+        if roundWinner:
             winnerCounter[roundWinner] += 1
 
     if winnerCounter['Maria'] > winnerCounter['Ben']:
@@ -20,37 +25,45 @@ def isWinner(x, nums):
 
 
 def isRoundWinner(n):
-    '''find round winner'''
-    list = [i for i in range(1, n + 1)]
+    '''Determines the winner of a single round'''
+    if n < 1:
+        return None
+
+    # List of numbers from 1 to n
+    numbers = list(range(1, n + 1))
     players = ['Maria', 'Ben']
     currentPlayerIndex = 0
 
     while True:
         currentPlayer = players[currentPlayerIndex]
         madeMove = False
-        for num in list:
+
+        for num in numbers:
             if isPrime(num):
-                list = [x for x in list if x % num != 0]
+                # Remove the prime number and its multiples
+                numbers = [x for x in numbers if x % num != 0]
                 madeMove = True
                 break
 
         if not madeMove:
+            # No move can be made, current player loses
             return players[1 - currentPlayerIndex]
 
+        # Switch to the other player
         currentPlayerIndex = 1 - currentPlayerIndex
 
 
-def isPrime(n):
-    '''check if number is prime'''
-    if n <= 1:
+def isPrime(num):
+    '''Check if a number is prime'''
+    if num <= 1:
         return False
-    if n <= 3:
+    if num <= 3:
         return True
-    if n % 2 == 0 or n % 3 == 0:
+    if num % 2 == 0 or num % 3 == 0:
         return False
     i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
             return False
         i += 6
     return True
